f40b24f7340506891283514bb187154064192b7d
Soroka3301 <sorokavova9a@gmail.com>
2023-02-15 16:45:40 +0200

    field

---
+++ Chess/Game.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "Chessman.h"
+#include <iostream> 
+
+class Game
+{
+	Chessman** field = new Chessman*[8];
+	Game()
+	{
+		for (int i = 0; i < 8; i++) {
+			field[i] = (Chessman*)malloc(8*sizeof(Chessman));
+		}
+
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+			
+			}
+		}
+	}
+	
+};
+
---
+++ Chess/Pawn.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "Chessman.h"
+class Pawn :
+    public Chessman
+{
+
+};
+
